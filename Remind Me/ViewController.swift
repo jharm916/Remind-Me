c48ca4fb3e18c604800555c403c0c3f1818ec68d
Jeremy Harman <Jeremy1@Jeremys-MBP.home>
2015-07-05 15:51:05 -0400

    Initial Commit

---
+++ Remind Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remind Me
+//
+//  Created by Jeremy Harman on 7/5/15.
+//  Copyright (c) 2015 Jeremy Harman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
